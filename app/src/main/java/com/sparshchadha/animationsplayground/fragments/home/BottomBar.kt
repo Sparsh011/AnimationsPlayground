873738686f9dec352c81d49fcb71ec2160219b98
Sparsh Chadha <93155464+Sparsh011@users.noreply.github.com>
2024-06-22 14:55:21 +0530

    bottom bar selection anim change

--- app/src/main/java/com/sparshchadha/animationsplayground/fragments/home/BottomBar.kt
+++ app/src/main/java/com/sparshchadha/animationsplayground/fragments/home/BottomBar.kt
@@ -1,17 +1,18 @@
 package com.sparshchadha.animationsplayground.fragments.home
 
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.outlined.Call
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material3.Icon
@@ -27,15 +28,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.delay
 
 @Composable
 internal fun HomeScreenBottomBar(
     modifier: Modifier,
     onIconIndexChange: (Int) -> Unit,
-    selectedIconIndex: Int
+    selectedIconIndex: Int,
 ) {
     Row(
         modifier = modifier
@@ -93,45 +92,47 @@ private fun BottomBarIcon(
     var showSelectionAnim by remember {
         mutableStateOf(false)
     }
-    val alpha by animateDpAsState(
-        targetValue = if (showSelectionAnim) -(5).dp else 0.dp,
-        animationSpec = spring(), label = ""
-    )
 
-    Column(
+    AnimatedContent(
+        targetState = isSelected,
+        label = "",
+        transitionSpec = {
+            if (isSelected) {
+                (fadeIn(animationSpec = spring()) +
+                        scaleIn(initialScale = 0.92f, animationSpec = spring()))
+                    .togetherWith(fadeOut(animationSpec = tween(90)))
+            } else {
+                (EnterTransition.None
+                    .togetherWith(ExitTransition.None))
+            }
+        },
         modifier = modifier
             .clickable {
                 if (!isSelected) showSelectionAnim = true
                 onClick()
             },
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        if (isSelected) {
-            LaunchedEffect(key1 = Unit) {
-                delay(300)
-                showSelectionAnim = false
-
+    ) { targetSelected ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LaunchedEffect(Unit) {
+                if (isSelected) {
+                    showSelectionAnim = false
+                }
             }
+
             Icon(
                 imageVector = icon,
                 contentDescription = null,
-                tint = Color.Red,
-                modifier = Modifier.offset(y = alpha)
+                tint = if (targetSelected) Color.Red else Color.LightGray,
             )
-        } else {
-            Icon(
-                imageVector = icon,
-                contentDescription = null,
-                tint = Color.LightGray,
+
+            Text(
+                text = text,
+                color = if (targetSelected) Color.Black else Color.LightGray,
+                fontWeight = if (targetSelected) FontWeight.Bold else FontWeight.Normal,
+                fontSize = 16.sp
             )
         }
-
-
-        Text(
-            text = text,
-            color = if (isSelected) Color.Black else Color.LightGray,
-            fontWeight = if (isSelected) FontWeight.Bold else FontWeight.Normal,
-            fontSize = 13.sp
-        )
     }
 }
